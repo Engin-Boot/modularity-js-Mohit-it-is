60eac170d4c83f5c6ab4fad28be178c33f0ae309
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-08-25 08:00:35 +0530

    updated module

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var testColorToNumber = require("./testColortoNumber.js");
 var testNumberToPair = require("./testNumbertoPair.js");
 const codingManual = require("./codingManual.js");
 
-<<<<<<< HEAD
-console.log(codingManual()); //outputs Manual
-=======
-//console.log(codingManual());
->>>>>>> d462be2b24bf29bf4d941ed597fe6bbe5e841870
+
+//console.log(codingManual()); //outputs Manual
+
 console.log("color coder");
 testNumberToPair(4, "white", "brown");
 testNumberToPair(25, "violet", "slate");
