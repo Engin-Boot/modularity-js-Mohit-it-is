e3a5822c4018b75182b4614eb23ffae486627609
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-08-31 10:41:12 +0530

    Reduced cyclomatic complexity

--- pairNumberFromColors.js
+++ pairNumberFromColors.js
@@ -1,19 +1,27 @@
 const {MajorColors,MinorColors} = require("./minor_major_color");
 
-
-function getPairNumberFromColors(majorColor, minorColor) {
+function getMajorIndex(majorColor){
     let majorIndex = 0;
-    let minorIndex = 0;
     for(majorIndex = 0; majorIndex < MajorColors.length; majorIndex++) {
         if(MajorColors[majorIndex] == majorColor) {
             break;
         }
     }
+    return majorIndex;
+}
+
+function getMinorIndex(minorColor){
+    let minorIndex = 0;
     for(minorIndex = 0; minorIndex < MinorColors.length; minorIndex++) {
         if(MinorColors[minorIndex] == minorColor) {
             break;
         }
     }
+    return minorIndex;
+}
+function getPairNumberFromColors(majorColor, minorColor) {
+    let majorIndex = getMajorIndex(majorColor);
+    let minorIndex = getMinorIndex(minorColor); 
     return majorIndex * MinorColors.length + minorIndex + 1;
 }
 
